5ce822cfb47267c48a0e2f65f0774efcbd5d43a6
Gitborlando <2629888564@qq.com>
2026-01-09 23:37:56 +0800

    1

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,17 +2,17 @@ export function twoDecimal(number: number) {
   return Number(number.toFixed(Number.isInteger(number) ? 0 : 2))
 }
 
+const compare = (a: any[], b: any[]) => {
+  if (a.length !== b.length) return false
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) return false
+  }
+  return true
+}
+
 export const memorized = <F extends (deps: any[]) => any>(func: F) => {
   let value: ReturnType<F>
   let lastDeps: any[] | undefined
-
-  const compare = (a: any[], b: any[]) => {
-    if (a.length !== b.length) return false
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) return false
-    }
-    return true
-  }
 
   return (deps: any[]) => {
     if (!lastDeps || !compare(lastDeps, deps)) {
