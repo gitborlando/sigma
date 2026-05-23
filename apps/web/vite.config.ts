e6c9bbf28eb98b636bffcf5b1686e9cd49cf2fc1
Panyang <2629888564@qq.com>
2026-05-24 00:57:44 +0800

    chore: initialize monorepo workspace

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig(() => {
     resolve: {
       alias: {
         src: path.resolve(__dirname, 'src'),
-        types: path.resolve(__dirname, 'types'),
+        types: path.resolve(__dirname, '../../types'),
       },
     },
     build: {
