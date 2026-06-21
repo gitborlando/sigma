c1d2f9c4efb7cc8fc1103c0212eceae1e606bdca
Gitborlando <2629888564@qq.com>
2026-06-21 18:21:44 +0800

    Remove unused sentence function and nano-string-utils dependency

--- apps/web/src/view/i18n/config.ts
+++ apps/web/src/view/i18n/config.ts
@@ -1,6 +1,5 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
-import { camelCase, pascalCase } from 'nano-string-utils'
 import { initReactI18next } from 'react-i18next'
 import en from './locales/en.json'
 import zh from './locales/zh.json'
@@ -38,42 +37,6 @@ export function setLanguage(language: string) {
 export function t(...args: Parameters<typeof i18n.t>) {
   getLanguage()
   return i18n.t(...args)
-}
-
-type TOptions = { space?: boolean; pascalCase?: boolean; camelCase?: boolean }
-
-export function sentence(...args: string[]): string
-export function sentence(...args: [...string[], TOptions]): string
-export function sentence(...args: string[] | [...string[], TOptions]) {
-  const language = getLanguage()
-
-  let words: string[] = []
-  let options: TOptions = {}
-
-  if (typeof args[args.length - 1] === 'object') {
-    words = args.slice(0, -1) as string[]
-    options = args[args.length - 1] as TOptions
-  } else {
-    words = args as string[]
-  }
-
-  const output = words.map((word) => {
-    if (options.pascalCase) {
-      return pascalCase(word)
-    }
-    return word
-  })
-
-  if (language === 'en' && !options.space) {
-    options.space = true
-  }
-  if (options.space && language !== 'zh') {
-    return output.join(' ')
-  }
-  if (options.camelCase) {
-    return camelCase(output.join(' '))
-  }
-  return output.join('')
 }
 
 export default i18n
