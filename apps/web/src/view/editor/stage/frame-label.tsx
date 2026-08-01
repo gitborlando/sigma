885f11f273357e860ee1281f9dad405b36c74c86
Gitborlando <2629888564@qq.com>
2026-08-01 19:23:49 +0800

    Make frame label display in right rotation an position

--- apps/web/src/view/editor/stage/frame-label.tsx
+++ apps/web/src/view/editor/stage/frame-label.tsx
@@ -1,7 +1,8 @@
 import { stopPropagation } from '@gitborlando/utils/browser'
+import { clamp } from 'es-toolkit'
 import { untracked } from 'mobx'
 import { Fragment } from 'react'
-import { Matrix } from 'src/editor/geometry'
+import { Matrix, MRect } from 'src/editor/geometry'
 import { SchemaHelper } from 'src/editor/schema/helper'
 import { useEditorServices } from 'src/view/hooks/editor'
 import { useShallow } from 'src/view/hooks/schema/use-shallow'
@@ -11,7 +12,7 @@ import { themeColor } from 'src/view/styles/color'
 
 const RULER_COLOR = '#9f9f9f'
 const FONT_SIZE = 14
-const LINE_HEIGHT = 14
+const LINE_HEIGHT = 18
 const TEXT_WIDTH = 54
 
 export const StageFrameLabelComp: FC<{}> = observer(({}) => {
@@ -46,15 +47,38 @@ const FrameLabelComp: FC<{ frame: S.Frame }> = observer(({ frame }) => {
   const [hovered, setHovered] = useState(false)
   const selected = useSelection()[frame.id]
 
+  const lineHeight = LINE_HEIGHT / zoom
+
   const textWidth =
     elemDrawer.textBreaker?.measureWidth(
       frame.name || 'no name',
       `500 ${FONT_SIZE}px GoogleSansCode`,
     ) || TEXT_WIDTH
 
-  const textMatrix = SchemaHelper.getAncestorMatrix(frame)
-    .append(Matrix.of(frame.matrix))
-    .append(Matrix.identity().shift({ x: 0, y: -(LINE_HEIGHT + 4) / zoom }))
+  const getTextMatrix = (rotation: number, x: number, y: number) =>
+    SchemaHelper.getAncestorMatrix(frame)
+      .append(Matrix.of(frame.matrix))
+      .append(Matrix.identity().rotate(rotation).shift({ x, y }))
+
+  const textMatrix1 = getTextMatrix(0, 0, -lineHeight)
+  const textMatrix2 = getTextMatrix(90, frame.width / zoom + lineHeight, 0)
+  const textMatrix3 = getTextMatrix(
+    180,
+    frame.width / zoom,
+    frame.height / zoom + lineHeight,
+  )
+  const textMatrix4 = getTextMatrix(270, -lineHeight, frame.height / zoom)
+
+  const textMatrix = [textMatrix1, textMatrix2, textMatrix3, textMatrix4].find(
+    (matrix) => {
+      const rotation = new MRect(100, 100, matrix, 1).rotation
+      return (
+        clamp(rotation, 0, 45) === rotation || clamp(rotation, 315, 360) === rotation
+      )
+    },
+  )
+
+  if (!textMatrix) return null
 
   const text = schemaCreator.text({
     id: `frame-label-${frame.id}`,
