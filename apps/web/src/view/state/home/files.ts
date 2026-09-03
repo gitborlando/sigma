35cc02abb2746ecd424de6c9301c8b03c7e6b21e
Panyang <2629888564@qq.com>
2026-09-03 16:44:08 +0800

    1

--- apps/web/src/view/state/home/files.ts
+++
@@ -1,9 +0,0 @@
-import { Service } from '@gitborlando/di-service'
-import autoBind from 'auto-bind'
-
-export class ViewStateFiles extends Service {
-  constructor() {
-    super()
-    autoBind(this)
-  }
-}
